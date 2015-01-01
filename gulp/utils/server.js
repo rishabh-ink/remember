ad27431c0093fe0bdd4a7e0737e834cdd9eacd41
Rishabh Rao <rishabhsrao@gmail.com>
2015-01-01 23:33:32 +0530

    Use Express as our web server on Heroku.

--- gulp/utils/server.js
+++ gulp/utils/server.js
@@ -1,9 +1,7 @@
 "use strict";
 
 var express = require("express");
-var path = require("path");
-var CFG = require("./config");
-
 var app = express();
+
 app.use(express.static("."));
 app.listen(process.env.PORT || 3000);
