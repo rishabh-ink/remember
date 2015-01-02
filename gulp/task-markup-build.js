d4d26ee9ef3b476cf4ca618e318353ad1b674215
Rishabh Rao <rishabhsrao@gmail.com>
2015-01-02 23:13:38 +0530

    Create markup:minify and markup:build tasks.

---
+++ gulp/task-markup-build.js
@@ -0,0 +1,29 @@
+var gulp = require("gulp");
+var CFG  = require("./utils/config.js");
+var $    = require("gulp-load-plugins")();
+var path = require("path");
+var pkg  = require(path.join("..", CFG.FILE.config.pkg));
+
+/**
+ * markup:build
+ * @see www.npmjs.org/package/useref
+ */
+gulp.task("markup:build", [], function () {
+  var assets = $.useref.assets({
+    searchPath: [
+      CFG.DIR.src
+    ]
+  });
+
+  var markupFilter = $.filter([
+    "*." + CFG.FILE.extension.markup.html
+  ]);
+
+  return gulp.src([
+      path.join(CFG.DIR.src, "*." + CFG.FILE.extension.markup.html)
+    ])
+    .pipe(assets)
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest(CFG.DIR.dist));
+});
--- gulp/task-markup-minify.js
+++ gulp/task-markup-minify.js
@@ -8,28 +8,14 @@ var pkg  = require(path.join("..", CFG.FILE.config.pkg));
  * markup:minify
  * @see www.npmjs.org/package/useref
  */
-gulp.task("markup:minify", [], function () {
-  var assets = $.useref.assets({
-    searchPath: [
-      "app"
-    ]
-  });
-
-  var markupFilter = $.filter([
-    "*." + CFG.FILE.extension.markup.html
-  ]);
-
+gulp.task("markup:minify", ["markup:build"], function () {
   return gulp.src([
-      path.join(CFG.DIR.src, "*." + CFG.FILE.extension.markup.html)
+      path.join(CFG.DIR.dist, "*." + CFG.FILE.extension.markup.html)
     ])
-    .pipe(assets)
-    .pipe(assets.restore())
-    .pipe($.useref())
-    // .pipe(markupFilter)
-    // .pipe($.minifyHtml({
-    //   conditionals: true,
-    //   loose: true
-    // }))
-    // .pipe(markupFilter.restore())
+    .pipe($.vulcanize({
+      dest: CFG.DIR.dist,
+      csp: true,
+      strip: true
+    }))
     .pipe(gulp.dest(CFG.DIR.dist));
 });
